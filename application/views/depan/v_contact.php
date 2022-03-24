60cdc179886c1114611fff0152a789eff0597dc1
SyahputraIndraPratama <syahputraindra034@gmail.com>
2022-03-24 11:38:09 +0700

    add project

---
+++ application/views/depan/v_contact.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Contact</title>
+    <link rel="shorcut icon" href="<?php echo base_url().'theme/images/tut-wuri-handayani-7759.png'?>">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/bootstrap.min.css'?>">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,700" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/font-awesome.min.css'?>">
+    <!-- Simple Line Font -->
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/simple-line-icons.css'?>">
+    <!-- Owl Carousel -->
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/owl.carousel.min.css'?>">
+    <!-- Main CSS -->
+    <link href="<?php echo base_url().'theme/css/style.css'?>" rel="stylesheet">
+</head>
+
+<body>
+  <!--============================= HEADER =============================-->
+  <div class="header-topbar">
+      <div class="container">
+          <div class="row">
+              <div class="col-xs-6 col-sm-8 col-md-9">
+                  <div class="header-top_address">
+                      <div class="header-top_list">
+                          <span class="icon-phone"></span>(0283) 355285
+                      </div>
+                      <div class="header-top_list">
+                          <span class="icon-envelope-open"></span>sman5kotategal@gmail.com
+                      </div>
+                      <div class="header-top_list">
+                          <span class="icon-location-pin"></span>Tegal, Jawa Tengah
+                      </div>
+                  </div>
+                  <div class="header-top_login2">
+                      <a href="<?php echo site_url('contact');?>">Hubungi Kami</a>
+                  </div>
+              </div>
+              <div class="col-xs-6 col-sm-4 col-md-3">
+                  <div class="header-top_login mr-sm-3">
+                      <a href="<?php echo site_url('contact');?>">Hubungi Kami</a>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+  <div data-toggle="affix" style="border-bottom:solid 1px #f2f2f2;">
+      <div class="container nav-menu2">
+          <div class="row">
+              <div class="col-md-12">
+                  <nav class="navbar navbar2 navbar-toggleable-md navbar-light bg-faded">
+                      <button class="navbar-toggler navbar-toggler2 navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown">
+                          <span class="icon-menu"></span>
+                      </button>
+                      <div class="logo">
+                            <h2>SMA Negeri 5 Tegal</h2>
+                        </div>
+                      <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+                          <ul class="navbar-nav">
+                              <li class="nav-item">
+                                  <a class="nav-link" href="<?php echo site_url('');?>">Home</a>
+                              </li>
+                              <li class="nav-item">
+                                  <a class="nav-link" href="<?php echo site_url('about');?>">About</a>
+                              </li>
+                              <li class="nav-item">
+                                  <a class="nav-link" href="<?php echo site_url('guru');?>">Guru</a>
+                              </li>
+                              <li class="nav-item">
+                                  <a class="nav-link" href="<?php echo site_url('blog');?>">Blog</a>
+                              </li>
+                              <li class="nav-item">
+                                  <a class="nav-link" href="<?php echo site_url('pengumuman');?>">Pengumuman</a>
+                              </li>
+                              <li class="nav-item">
+                                  <a class="nav-link" href="<?php echo site_url('agenda');?>">Agenda</a>
+                              </li>
+                              <li class="nav-item">
+                                  <a class="nav-link" href="<?php echo site_url('galeri');?>">Galeri</a>
+                              </li>
+                              <li class="nav-item">
+                                <a class="nav-link" href="<?php echo site_url('contact');?>">Kontak</a>
+                              </li>
+                        </ul>
+                  </div>
+                </nav>
+              </div>
+            </div>
+          </div>
+        </div>
+    <section>
+</section>
+<!--//END HEADER -->
+<section class="contact">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="contact-title">
+                    <h2>Hubungi Kami</h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="contact-form">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-12 col-md-6 contact-option">
+                            <div class="contact-option_rsp">
+                                <h3>Tinggalkan Pesan</h3>
+                                <form action="<?php echo site_url('contact/kirim_pesan');?>" method="post">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" placeholder="Name" name="xnama" required>
+                                    </div>
+                                    <!-- // end .form-group -->
+                                    <div class="form-group">
+                                        <input type="email" class="form-control" placeholder="Email" name="xemail" required>
+                                    </div>
+                                    <!-- // end .form-group -->
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" placeholder="Phone" name="xphone" required>
+                                    </div>
+                                    <!-- // end .form-group -->
+                                    <div class="form-group">
+                                        <textarea placeholder="Message" class="form-control" name="xmessage" required rows="5"></textarea>
+                                    </div>
+                                    <!-- // end .form-group -->
+                                    <button type="submit" class="btn btn-default btn-submit">SUBMIT</button>
+                                    <div><?php echo $this->session->flashdata('msg');?></div>
+                                    <!-- // end #js-contact-result -->
+                                </form>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-12 col-md-6">
+                            <div class="contact-address">
+                                <h3>Lokasi</h3>
+                                <div class="contact-details">
+                                    <i class="fa fa-user" aria-hidden="true"></i>
+                                    <h6>Alamat</h6>
+                                    <p> Jl. Kali Kemiri II, Margadana 
+                                        <br> Kec. Margadana, Kota Tegal
+                                        <br> Jawa Tengah 52143</p>
+                                    </div>
+                                    <br>
+                                    <div class="contact-details">
+                                        <i class="fa fa-envelope-o" aria-hidden="true"></i>
+                                        <h6>Email</h6>
+                                        <p>sman5kotategal@gmail.com
+                                        </p>
+                                    </div>
+                                    <br>
+                                    <div class="contact-details">
+                                        <i class="fa fa-phone" aria-hidden="true"></i>
+                                        <h6>Phone</h6>
+                                        <p>(0283) 355285</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <p class="contact-center">OR</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--//END  ABOUT IMAGE -->
+    <!--============================= FOOTER =============================-->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="foot-logo">
+                        <a href="<?php echo site_url();?>">
+                            <img width="150px" src="<?php echo base_url().'theme/images/tut-wuri-handayani-7759.png'?>" class="img-fluid" alt="footer_logo">
+                        </a>
+                        <p><?php echo date('Y');?> © copyright by Kelompok Program Pengabdian Masyarakat</p>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="sitemap">
+                            <h3>Menu Utama</h3>
+                            <ul>
+                                <li><a href="<?php echo site_url();?>">Home</a></li>
+                                <li><a href="<?php echo site_url('about');?>">About</a></li>
+                                <li><a href="<?php echo site_url('artikel');?>">Blog </a></li>
+                                <li><a href="<?php echo site_url('galeri');?>">Galeri</a></li>
+                                <li><a href="<?php echo site_url('contact');?>">Kontak</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                      <div class="sitemap">
+                          <h3>Akademik</h3>
+                          <ul>
+                              <li><a href="<?php echo site_url('guru');?>">Guru</a></li>
+                              <li><a href="<?php echo site_url('pengumuman');?>">Pengumuman</a></li>
+                              <li><a href="<?php echo site_url('agenda');?>">Agenda</a></li>
+                          </ul>
+                      </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="address">
+                            <h3>Hubungi Kami</h3>
+                            <p><span>Alamat: </span> Jl. Kali Kemiri II, Margadana, Kec. Margadana, Kota Tegal, Jawa Tengah 52143</p>
+                            <p>Email : sman5kotategal@gmail.com
+                                <br> Phone : (0283) 355285</p>
+                                <ul class="footer-social-icons">
+                                    <li><a href="#"><i class="fa fa-facebook fa-fb" aria-hidden="true"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-linkedin fa-in" aria-hidden="true"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-twitter fa-tw" aria-hidden="true"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+            <!--//END FOOTER -->
+            <!-- jQuery, Bootstrap JS. -->
+            <script src="<?php echo base_url().'theme/js/jquery.min.js'?>"></script>
+            <script src="<?php echo base_url().'theme/js/tether.min.js'?>"></script>
+            <script src="<?php echo base_url().'theme/js/bootstrap.min.js'?>"></script>
+            <script src="<?php echo base_url().'theme/js/owl.carousel.min.js'?>"></script>
+            <script src="<?php echo base_url().'theme/js/validate.js'?>"></script>
+            <script src="<?php echo base_url().'theme/js/tweetie.min.js'?>"></script>
+            <!-- Subscribe / Contact-->
+            <script src="<?php echo base_url().'theme/js/subscribe.js'?>"></script>
+            <script src="<?php echo base_url().'theme/js/contact.js'?>"></script>
+            <!-- Script JS -->
+            <script src="<?php echo base_url().'theme/js/script.js'?>"></script>
+        </body>
+
+        </html>
